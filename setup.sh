1f9a5a46c57c73b86ecf4dc20031f2263aeed00a
ayushrai139 <72242073+ayushrai139@users.noreply.github.com>
2022-12-21 14:27:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"darekarabhishek@gmail.com\"\n\
+email = \"ayushrai139@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
